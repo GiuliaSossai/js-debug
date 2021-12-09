afc835c176dfc38be58334d894de63de2ef11d1b
GiuliaSossai <giulia.sossai@yahoo.it>
2021-12-09 16:23:35 +0100

    bundle 1 e 2

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -10,38 +10,81 @@
 *******************************************************************************/
 
 
-// ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
-    console.log(i);
-}
+// ESERCIZIO 1:
+// è un ciclo for
+// c'è un errore logico: se i=0 non può essere anche maggiore di 5
+
+// for (let i = 0; i > 5; i++) {
+//     console.log(i);
+// }
+
+for (let i = 0; i < 5; i++) {
+        console.log('es 1:', i);
+    }
 
 
 // ESERCIZIO 2
+// è una funzione che verifica se il numero che indico come parametro è un numero pari; se lo è, restituisco il numero più cinque
+// l'errore è di logica e di sintassi: dentro la condizione ci deve essere un'uguaglianza
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {
+        console.log('numero', num);
         return num + 5;
     }
-    return num;
+    //return num;
 }
+
+console.log('funzione', addIfEven(8));
 
 
 // ESERCIZIO 3
+// è una funzione con ciclo for
+// errore di sintassi: al posto delle virgole bisogna mettere il punto e virgola
+//errore di logica: i deve essere minore o uguale a 5 visto che la funzione si chiama loop to five, altrimenti il ciclo arriverebbe fino a 4
+
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
+
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i <= 5; i++) {
         console.log(i);
     }
 }
-
+loopToFive();
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+//è una funzione che a partire da un array di numeri restituisce l'array dei numeri pari presenti nell'array padre
+// 1-2: dentro ciclo for c'è un ; di troppo alla fine e i deve essere minore o uguale;
+// 3: dentro la condizione if ci deve essere un'uguaglianza
+// 4: il return va fuori dal ciclo for
+// 5-6: al push e nell'if non ci va i ma ci va numbers[i]
+// 7: c'è ; di troppo dopo la condizione if
+
+
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
-        }
-        return evenNumbers;
+    for (let i = 0; i <= numbers.length - 1; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
+        }    
     }
+    return evenNumbers;
 }
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+displayEvenNumbers();
+console.log(displayEvenNumbers());
--- bundle-02/main.js
+++ bundle-02/main.js
@@ -10,58 +10,69 @@
 *******************************************************************************/
 
 
-// ESERCIZIO 1
+// ESERCIZIO 1:
+// errore logico: message deve essere una variabile let perché cambia a seconda del caso
 function checkAge() {
-    const myAge = 32;
-    const message = '';
+    const myAge = 12;
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+
+    console.log(message);
 }
 checkAge();
 
+
 // ESERCIZIO 2
+// errore di sintassi: lenght è scritto male
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
 
 
 // ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// errore di logica e sintassi: dal momento che devo inserire un numero, dopo il prompt devo aggiungere il metodo parseInt
+// function addNumbers() {
+//     const userNumber = prompt(parseInt('Inserisci un numero'));
+//     const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
 
 // ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+// errore di sintassi e di logica: i valori booleani non hanno bisogno degli apici
 
-    let grantAccess = 'false';
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
+//     let grantAccess = false;
 
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = true;
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+
+// checkAccess();
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
+//errore di sintassi: manca la chiusura della parentesi graffa dell'intera funzione
+
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
@@ -70,50 +81,21 @@ function checkAccessImproved() {
     let grantAccess = false;
 
     for (let i = 0; i < addresses.length; i++) {
-     const email = addresses[i];
+        const email = addresses[i];
         
-      if (userEmail.length > 5) {
+        if (userEmail.length > 5) {
             
-        if (email === userEmail) {
-        grantAccess = 'true';            
-            
-      }
+            if (email === userEmail) {
+            grantAccess = 'true';                
+            }
         
-    }
+        }
 
-    if (grantAccess) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
+        if (grantAccess) {
+            console.log('Accesso consentito!');
+        } else {
+            console.log('Accesso negato!');
+        }
     }
 }
 checkAccessImproved();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
